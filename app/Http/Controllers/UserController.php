bc0aee8f5f9179bbb32c63b505bc9dd63bf59ae3
Guilherme Alves <guigaanjos@hotmail.com>
2023-02-28 20:08:41 -0300

    Refatorando a Controller de usuário e os elementos reutilizáveis do frontend para componente

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,29 +8,29 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    protected $model;
+
+    public function __construct(User $user)
+    {
+        $this->model = $user;
+    }
+
     public function index(Request $request) 
     {
         // Opção filtrando apenas pelo nome
-        //$users = User::where('name', 'LIKE', "%{$request->search}%")->get();        
+        //$users = $this->model->where('name', 'LIKE', "%{$request->search}%")->get();        
 
         // Opção com filtro por nome ou e-mail
-        $search = $request->search;
-
-        $users = User::where(function ($query) use ($search) {
-            if ($search) {
-                $query->where('email', $search);
-                $query->orWhere('name', 'LIKE', "%{$search}%");
-            }
-        })->get();
+        $users = $this->model->getUsers(search: $request->search ?? '');
 
         return view('users.index', compact('users'));
     }
 
     public function show($id) 
     {
-        // $user = User::where('id', $id)->first(); (Uma opção)
+        // $user = $this->model->where('id', $id)->first(); (Uma opção)
 
-        if(!$user = User::find($id))
+        if(!$user = $this->model->find($id))
             return redirect()->route('users.index');        
         
         return view('users.show', compact('user'));
@@ -46,7 +46,7 @@ class UserController extends Controller
         $data = $request->all();
         $data['password'] = bcrypt($request->password);
 
-        User::create($data);
+        $this->model->create($data);
 
         return redirect()->route('users.index');
         
@@ -60,7 +60,7 @@ class UserController extends Controller
 
     public function edit($id)
     {
-        if(!$user = User::find($id))
+        if(!$user = $this->model->find($id))
             return redirect()->route('users.index'); 
     
         return view('users.edit', compact('user'));
@@ -68,7 +68,7 @@ class UserController extends Controller
 
     public function update(StoreUpdateUserFormRequest $request, $id)
     {
-        if(!$user = User::find($id))
+        if(!$user = $this->model->find($id))
             return redirect()->route('users.index'); 
         
         $data = $request->only('name', 'email');
@@ -83,7 +83,7 @@ class UserController extends Controller
 
     public function delete($id)
     {
-        if(!$user = User::find($id))
+        if(!$user = $this->model->find($id))
             return redirect()->route('users.index'); 
     
         $user->delete();
