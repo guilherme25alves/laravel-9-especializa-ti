7e3cd05edb891028489ada104f8415911e16f71c
Guilherme Alves <guigaanjos@hotmail.com>
2023-03-04 13:05:34 -0300

    Implementado autenticação e login com Laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\{
     UserController
 };
@@ -17,25 +18,47 @@ use App\Http\Controllers\Admin\CommentController;
 |
 */
 
+/*
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+*/
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/users/{id}/comments/create', [CommentController::class, 'create'])->name('comments.create');
-Route::get('/users/{userId}/comments/{id}', [CommentController::class, 'edit'])->name('comments.edit');
-Route::put('/comments/{id}', [CommentController::class, 'update'])->name('comments.update');
-Route::post('/users/{id}/comments', [CommentController::class, 'store'])->name('comments.store');
-Route::get('/users/{id}/comments', [CommentController::class, 'index'])->name('comments.index');
+// Aplicando Middleware em um grupo de rotas
 
-Route::delete('/users/{id}', [UserController::class, 'delete'])->name('users.delete');
-Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
-Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
-/** 
- * WARNING: Importante colocar as rotas com parâmetros embaixo para que o Laravel não entenda 
- *  que uma outra rota seja parâmetro, exemplo da users/{id} e users/create, o Laravel poderia 
- *  entender que "create" era o valor do parâmetro, colocando a rota /create acima, evitamos esse problema
-*/
-Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
-Route::post('/users', [UserController::class, 'store'])->name('users.store');
-Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
+Route::middleware(['auth'])->group(function () {
+
+    Route::get('/users/{id}/comments/create', [CommentController::class, 'create'])->name('comments.create');
+    Route::get('/users/{userId}/comments/{id}', [CommentController::class, 'edit'])->name('comments.edit');
+    Route::put('/comments/{id}', [CommentController::class, 'update'])->name('comments.update');
+    Route::post('/users/{id}/comments', [CommentController::class, 'store'])->name('comments.store');
+    Route::get('/users/{id}/comments', [CommentController::class, 'index'])->name('comments.index');
+
+    Route::delete('/users/{id}', [UserController::class, 'delete'])->name('users.delete');
+    Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
+    Route::get('/users/{id}/edit', [UserController::class, 'edit'])->name('users.edit');
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    /** 
+     * WARNING: Importante colocar as rotas com parâmetros embaixo para que o Laravel não entenda 
+     *  que uma outra rota seja parâmetro, exemplo da users/{id} e users/create, o Laravel poderia 
+     *  entender que "create" era o valor do parâmetro, colocando a rota /create acima, evitamos esse problema
+    */
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::post('/users', [UserController::class, 'store'])->name('users.store');
+    // Implementando middleware (filtro) diretamente na rota 
+    Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show')->middleware('auth'); 
+});
+
+
+
+require __DIR__.'/auth.php';
